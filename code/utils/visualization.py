36e555b2f0510ecd987536605cf1bf55f59203dc
Siedrid <laura.obrecht@gmx.de>
2025-04-11 10:28:00 +0200

    added mAP per class

--- code/utils/visualization.py
+++ code/utils/visualization.py
@@ -68,14 +68,14 @@ def plot_sample(df, ROOT, n=1):
 
     plt.show()
 
-def plot_class_accuracy(df):
-    df = df.sort_values(by="Accuracy", ascending=False)
+def plot_class_mAP(df):
+    df = df.sort_values(by="AP", ascending=False)
     colors = [hotkey_to_color[int(class_id)] for class_id in df.Class]
     class_names = [class_name[int(class_id)] for class_id in df.Class]
 
     fig, axes = plt.subplots(figsize=(13,6))
-    axes.bar(class_names, df.Accuracy, color = colors)
-    axes.set_title("Per Class Accuracy")
+    axes.bar(class_names, df.AP, color = colors)
+    axes.set_title("Per Class Average Precision")
     axes.tick_params("x", rotation=45)
     plt.tight_layout()
     plt.show()
